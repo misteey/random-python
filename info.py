1b6249113e098d2a59496e09044ad821004ae523
Ana <lainrpgs@gmail.com>
2022-05-05 11:26:58 +0200

    Create info.py

---
+++ info.py
@@ -0,0 +1,8 @@
+import sys
+import keyword
+print(sys.version-info)
+print(" Python programming language")
+print(" Guido van Rossum, " + '1989.')
+print(' https://www.python.org/downloads/')
+print(' help' + chr(64) + 'python.org')
+print(keyword.kwlist)
