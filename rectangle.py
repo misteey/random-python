d5c91e0f6f4f8b26250d3d3a69de76e466f1624c
Ana <lainrpgs@gmail.com>
2022-05-06 18:10:52 +0200

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -6,6 +6,6 @@ def main():
   diagonal = (a ** 2 + b ** 2) ** 0.5
   
   print(" length=", a, "\t width=", b)
-  print(" area=", area, '\n perimeter=", perimeter)
+  print(" area=", area, "\n perimeter=", perimeter)
   print(" diagonal= %.2f" % d)
 main()
