8dd39f583c50fcfc5e3c73b027d745bac2897d80
Ana <lainrpgs@gmail.com>
2022-05-06 18:10:23 +0200

    Create rectangle.py

---
+++ rectangle.py
@@ -0,0 +1,11 @@
+def main():
+  a = float(input("length = "))
+  b = float(input("width = "))
+  perimeter = 2 * (a + b)
+  area = a * b
+  diagonal = (a ** 2 + b ** 2) ** 0.5
+  
+  print(" length=", a, "\t width=", b)
+  print(" area=", area, '\n perimeter=", perimeter)
+  print(" diagonal= %.2f" % d)
+main()
